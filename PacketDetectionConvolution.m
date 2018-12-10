f1a047470c1cc24c618ecc5146fa338e7a17feba
panvaf <vafeidis@campus.tu-berlin.de>
2018-12-10 12:45:50 +0100

    Detection based on local estimate of population activity

--- PacketDetectionConvolution.m
+++ PacketDetectionConvolution.m
@@ -1,6 +1,6 @@
 %% GET MATRIX OF SPIKE TIMES
 include_MUA = false;
-add_random_jitter = true; % better to have MUA on false when this is true
+add_random_jitter = false; % better to have MUA on false when this is true
 jitter_range = 1; % in s
 
 load CellParams.mat
@@ -37,27 +37,42 @@ bin_size = 0.001;
 nBins = floor(rec_length/bin_size);
 
 %% Parameters for detection, need to be tuned separately for each dataset
-kernel_size = 0.02; % in sec, gaussian kernel
-perc = 1; % percentage of average population activity that corresponds to a packet
-isclose = 0.025; % in sec, merge events that are too close
+kernel_size = 0.04; % in sec, gaussian kernel
+perc = .6; % percentage of average local population activity that corresponds to a packet
+win = 5*60; % size of local window, in seconds
+isclose = 0.035; % in sec, merge events that are too close
 min_duration = 0.05; % in sec, look for silent periods before and after packet
-th = .15; % percentage of maximum of packet that must be reached by a quiet in-between state
-min_dur = 0.02; % in sec, delete packets that are smaller than this
+th = .08; % percentage of maximum of packet that must be reached by a quiet in-between state
+min_dur = 0.04; % in sec, delete packets that are smaller than this
+max_dur = 0.5; % in sec, delete packets that are bigger than this
 
 % initial params (Tuesday): .1,1.5,.025,.05,.4,.02
 % got quite good diagrams (Wednesday) with 0.07, 1.3, 0.025, 0.05, 0.3, 0.02
 % better detection with 0.07, 1.2, 0.025, 0.05, 0.2, 0.02, still some
 % packets missing
 
+% Noam's parameters (Thursday): 0.02, 1, .025, .05, .05, .02, .5 a lot of false
+% positives
+
+% 0.02, 1.2, .025, .05, .05, .02, .5 is basically a coincidence detector
+
+% 0.04, 1, 0.035, .05, .08, .04, .5 is a good compromise between picking
+% up small packets and avoiding coincidence detection. could lower th a
+% little more if we want to be more robust at the cost of losing packets
+
+% LR1: more strict threshold on silence because a lot of asynchronous
+% activity? chose to increase the threshold for detection:
+% 0.04, 1.2, 0.035, .05, .08, .04, .5
 
 %% define kernel
 
 % 50 ms min packet size in literature, integrate over longer time to reduce the effect of coincidences
-kernel_size = kernel_size/bin_size;
+kernel_size = floor(kernel_size/bin_size);
 kernel = gausswin(kernel_size);
 
 %% convolve with kernel 
 
+win = floor(win/bin_size);
 syn = zeros(1,nBins);
 spikes_hist = hist(spiketimesArray,nBins);
 clear spiketimesArray
@@ -65,10 +80,10 @@ clear spiketimesArray
 pop = filter(kernel,1,spikes_hist);
 % pop = zscore(pop); % does not increase discrimination in a meaningfull way
 
-thresh = perc*mean(pop); % set threshold to percentage of maximum
+thresh = perc*movmean(pop,win); % set threshold to percentage local average
 
 for i=1:size(pop,2)
-    if pop(i) > thresh
+    if pop(i) > thresh(i)
         syn(i) = 1;
     end
 end
@@ -83,7 +98,7 @@ off = find(transitions==-1)';
 
 %% if two packets are too close, collate them
 
-isclose = isclose/bin_size;
+isclose = floor(isclose/bin_size);
 
 for i=2:size(off)
     if on(i)-off(i-1)<isclose
@@ -98,7 +113,7 @@ off(off==0) = [];
 %% if a packet is not prominent enough, delete it
 % must always be done after packet collation! (because otherwise we might lose part of a packet)
 
-min_duration = min_duration/bin_size;
+min_duration = floor(min_duration/bin_size);
 
 for i=2:size(off)-1
     max_in = max(pop(on(i):off(i)));
@@ -113,12 +128,14 @@ end
 on(on==0) = [];
 off(off==0) = [];
 
-%% delete packets that are too small
+%% delete packets that are too small or too large
 
-min_dur = min_dur/bin_size;
+min_dur = floor(min_dur/bin_size);
+max_dur = floor(max_dur/bin_size);
 
 for i=1:size(off)
-    if off(i) - on(i) < min_dur
+    dif = off(i) - on(i);
+    if dif < min_dur | dif > max_dur
         on(i) = 0;
         off(i) = 0;
     end
@@ -138,7 +155,7 @@ alls(:,2)=[];
 INX(:,1) = on; 
 INX(:,2) = off; 
 
-INX = INX + f(1) - 1 - kernel_size/2 +50; % add the time we cut so indices are aligned properly ,
+INX = INX + f(1) - 1 - kernel_size/2; % add the time we cut so indices are aligned properly ,
 
 % add offset of ~50ms in DSC4307_181016_1_RSC file
 
--- crosscor.m
+++ crosscor.m
@@ -13,9 +13,16 @@ stmFil = [basepath '/' basename '.evt.stm'];
 stm_evs = LoadEvents(stmFil);
 stm_st = stm_evs.time(cellfun(@any,regexp(stm_evs.description,'start')));
 
+cch1 = CrossCorr(rip_st, uds_st, 0.01, 100);
+cch1 = cch1./0.01./length(rip_st);
+cch2 = CrossCorr(stm_st, uds_st, 0.01, 100);
+cch2 = cch2./0.01./length(stm_st);
 
+timevec = linspace(-.5 , .5, 101);
+bar(timevec,smooth(cch1))
+figure
+bar(timevec,smooth(cch2))
 
-cch = CrossCorr(rip_st, uds_st, 0.01, 100);
-cch = CrossCorr(stm_st, uds_st, 0.01, 100);
-
-
+save(['/home/panteleimon/Documents/diagrams/' basename '.mat'], 'cch1','cch2')
+saveas(figure(1),['/home/panteleimon/Documents/diagrams/' basename '_uds_rip_cch.fig'])
+saveas(figure(2),['/home/panteleimon/Documents/diagrams/' basename '_uds_stim_cch.fig'])
